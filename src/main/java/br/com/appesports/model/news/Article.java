efce35f59c520cbc6de2f4d099c5f5ef9d19f3b4
Lucas Fonseca <fonsekalucas@gmail.com>
2019-12-04 00:05:33 -0300

    Implementação de novas features:
    ## Criação de scripts da tabela Category;
    ## Adição de configurações no application.properties;
    ## Adição de annotations de mapeamento de banco de dados do JPA.

--- src/main/java/br/com/appesports/model/news/Article.java
+++ src/main/java/br/com/appesports/model/news/Article.java
@@ -1,27 +1,36 @@
 package br.com.appesports.model.news;
 
-import br.com.appesports.model.news.enums.Category;
-
 import br.com.appesports.model.reporter.ReporterName;
-import java.util.Date;
-import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity(name = "article")
 public class Article {
 
+  @Column(name = "idArticle")
   private String idArticle;
 
+  @Column(name = "title")
   private String title;
 
+  @Column(name = "description")
   private String description;
 
+  @Column(name = "category")
   private Category category;
 
+  @Column(name = "bodyArticle")
   private String bodyArticle;
 
+  @Column(name = "reporterName")
   private ReporterName reporterName;
 
+  @Column(name = "publicationDate")
   private Date publicationDate;
 
+  @Column(name = "image")
   private Image image;
 
 }
---
+++ src/main/java/br/com/appesports/model/news/Category.java
@@ -0,0 +1,9 @@
+package br.com.appesports.model.news;
+
+public class Category {
+
+    private Integer idCategory;
+
+    private String categoryName;
+
+}
