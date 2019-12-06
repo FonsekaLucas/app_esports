22767f961f22649b733547182dcc5306dd9378f1
Lucas Fonseca <fonsekalucas@gmail.com>
2019-12-06 00:14:24 -0300

    Configuração das entidades pertencentes ao contexto da notícia.

--- src/main/java/br/com/appesports/model/news/Category.java
+++ src/main/java/br/com/appesports/model/news/Category.java
@@ -1,9 +1,47 @@
 package br.com.appesports.model.news;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity(name = "category")
 public class Category {
 
+    @Id
+    @Column(name = "idCategory")
     private Integer idCategory;
 
+    @Column(name = "categoryName")
     private String categoryName;
 
+    public Integer getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(Integer idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(idCategory, category.idCategory) &&
+                Objects.equals(categoryName, category.categoryName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCategory, categoryName);
+    }
 }
